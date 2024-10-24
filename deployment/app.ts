2942940f1cd12841609ed658421b2f5b98039aef
Madhan Kumar Tachyon Systems <99461480+Madhankumar-1@users.noreply.github.com>
2024-10-24 11:59:44 +0530

    Update app.ts

--- deployment/app.ts
+++ deployment/app.ts
@@ -26,3 +26,98 @@ export function deploy(extendedGroupEndpoints: TYPES.ExtendedGroupEndpoints) {
      }
      
 }
+
+const data = {
+     "dummy-api-gen-api-stack": {
+          "dummy-api-gateway": {
+               "account": "1949874554",
+               "region": "ap-southeast-2",
+               "separateHostedZones": false,
+               "ssmKeys": {
+                    "git": {
+                         "gitTokenSsmPath": "/git/gitToken",
+                         "gitUsernameKeySsmPath": "/git/gitUsername",
+                         "userNameSsmPath": "/git/userName",
+                         "proxyDomainSsmPath": "/git/gitProxyDomain"
+                    },
+                    "jobs": {"templateRepoKeySsmPath": "/jobs/api/templateRepoName"},
+                    "chatAgent": {"chatAgentKeySsmPath": "/chatAgent/chatAgentDomain"}
+               },
+               "singleRepo": false,
+               "repoName": "cnyp-8mfj3gpuhd",
+               "productShortName": "cny",
+               "fileLocation": "Local",
+               "resources": {},
+               "features": {
+                    "Authentication": {
+                         "Name": "Authentication",
+                         "Props": "key"
+                    },
+                    "ErrorHandler": {
+                         "Name": "ErrorHandler",
+                         "Props": "key"
+                    },
+                    "DiscoveryService": {
+                         "key": "adra"
+                    }
+               },
+               "schemas": {
+                    "process": "process.js"
+               },
+               "schemaFileBasePath": "../pipeline/requirements/schemas",
+               "responseFileBasePath": "../pipeline/requirements/responses",
+               "endpointsInfoArray": [
+                    {
+                         "projectName": "cogniyonPlatform",
+                         "projectShortName": "cnyp",
+                         "resourceName": "dummy",
+                         "path": "/start",
+                         "httpMethod": "POST",
+                         "id": 1,
+                         "singleRepo": false,
+                         "distributed": false,
+                         "stage": "dev",
+                         "deploymentGroup": "api-gen-api-stack",
+                         "gatewayGroup": "api-gateway",
+                         "apiGatewayType": "http",
+                         "serverUrl": "api-dev.talksite.ai",
+                         "supportedSignIns": [],
+                         "version": "v1",
+                         "pathPrefix": "/jobs",
+                         "requestQueryParams": null,
+                         "requestHeaders": null,
+                         "requestBody": null,
+                         "responseHeader": null,
+                         "responseBody": null,
+                         "language": "nodejs",
+                         "envVariables": ["DynamoDBTable", "Stage"],
+                         "responsesFile": "apigen-job-producer.responses.json",
+                         "serverType": "AwsServerless",
+                         "dependenciesRef": [],
+                         "featuresRef": ["Authentication", "ErrorHandler", "DiscoveryService"],
+                         "outputType": "Json",
+                         "repoName": "cnyp-8mfj3gpuhd",
+                         "endpointId": "cogniyonPlatform-8mfj3gpuhd-1-1",
+                         "responseType": "Single",
+                         "serviceMethodName": "apigen-job-producer",
+                         "projectId": "8mfj3gpuhd",
+                         "serviceId": "cnyp-apigen-job-producer",
+                         "stackId": "",
+                         "apigatewayId": "",
+                         "apigatewayUrl": "",
+                         "apiType": "ApiLambdaHelloWorld",
+                         "operation": "Create",
+                         "schemasRef": ["apigen"],
+                         "sourceHttpMethod": "Create",
+                         "permissions": [{"type": "AwsSQS", "name": "apigen-job-queue", "access": "SEND_MESSAGE"}]
+                    }
+               ],
+               "stage": "dev",
+               "outputType": "Json",
+               "serverUrl": "api-dev.talksite.ai"
+          }
+     }
+}
+
+
+deploy(data)
